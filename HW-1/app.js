102e7ed2000641e2c5b7db3d52fc41f6639a2181
Rishat29 <46088668+Rishat29@users.noreply.github.com>
2023-04-05 23:09:12 +0600

    Домашняя Работа - 1

---
+++ HW-1/app.js
@@ -0,0 +1,47 @@
+//if,else if, else
+
+// var grade = (prompt('Enter your grade'))
+// if (grade > 5){
+//     alert("Спасибо за высокую оценку")
+// }else if (grade < 5){
+//     alert("Мы работаем над улучшением сервиса!")
+// }else {
+//     alert("!ОШИБКА!")
+// }
+
+//switch
+
+
+// var grade = Number(prompt('Enter your grade'))
+// switch (grade) {
+//     case 5:
+//     case 6:
+//     case 7:
+//     case 8:
+//     case 9:
+//     case 10:
+//         alert("Спасибо за высокую оценку")
+//         break
+//     case 4:
+//     case 3:
+//     case 2:
+//     case 1:
+//         alert("Мы работаем над улучшением сервиса!")
+//         break
+// }
+
+//
+
+// let language = prompt("Выберите язык: 'ENG' или 'RUS'");
+//
+// switch (language) {
+//     case "ENG":
+//         console.log(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]);
+//         break;
+//     case "RUS":
+//         console.log(["пн", "вт", "ср", "чт", "пт", "сб", "вс"]);
+//         break;
+//     default:
+//         console.log("Ошибка: неправильно выбран язык.");
+//         break;
+// }
