fc2ef088251541b3f39c6837b150bc90b667b98a
Rishat29 <46088668+Rishat29@users.noreply.github.com>
2023-04-09 04:03:50 +0600

    Домашняя работа 2

---
+++ HW2/app.js
@@ -0,0 +1,48 @@
+//1
+
+// var arr = [10, 20, 30, 50, 235, 3000];
+//
+// for (let i = 0; i < arr.length; i++) {
+//     const str = String(arr[i]);
+//     if (str[0] === '1' || str[0] === '2' || str[0] === '5') {
+//         console.log(arr[i]);
+//     }
+// }
+
+
+//2
+
+// var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+// console.log(arr)
+// console.log(arr.reverse())
+
+//3
+
+// var colors = {
+//     red: 'стоп',
+//     yellow: 'готовьтесь',
+//     green: 'идите'
+// }
+//
+// var color = prompt('Введите цвет светофора на английском', '');
+//
+// if (color === 'red') {
+//     alert(colors[color]);
+// } else if (color === 'yellow') {
+//     alert(colors[color]);
+// } else if (color === 'green') {
+//     alert(colors[color]);
+// } else {
+//     alert('Введите цвет светофора: red, yellow или green');
+// }
+
+
+//4
+//
+// var arr = [];
+//
+// for (var i = 0; i <= 10; i++) {
+//     arr.push(i);
+// }
+//
+// console.log(arr);
